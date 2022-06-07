2615bf69108f065bd53d9868bbaeac2968a0772b
ThoreFrenzel <you@example.com>
2022-06-07 20:19:18 +0200

    1- refactoring for PR

--- test-application-be/src/main/java/com/example/testapplicationbe/model/datamodel/Variant.java
+++ test-application-be/src/main/java/com/example/testapplicationbe/model/datamodel/Variant.java
@@ -14,7 +14,7 @@ import java.io.Serializable;
 public class Variant implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
     @Column(name = "name")
