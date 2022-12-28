44bf9a43885606eebceb7900d13fcedba7a57a28
Ruben Smidt <ruben@rsmidt.dev>
2022-12-28 15:02:52 +0100

    chore!: upgrade to spring boot 3

--- app/src/main/java/de/snookersbuddy/snookersbuddyserver/ports/rest/order/InitOrderInput.java
+++ app/src/main/java/de/snookersbuddy/snookersbuddyserver/ports/rest/order/InitOrderInput.java
@@ -2,7 +2,7 @@ package de.snookersbuddy.snookersbuddyserver.ports.rest.order;
 
 import de.snookersbuddy.snookersbuddyserver.domain.model.assignment.Assignment;
 
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotNull;
 
 public record InitOrderInput(@NotNull Assignment assignment) {
 }
