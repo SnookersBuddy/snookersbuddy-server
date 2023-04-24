e9478666bfcf1b1a4ccc2eadc2c9be29848ba8f2
jlkDE <me@jlk.one>
2023-04-24 22:00:52 +0200

    feat: allow clean and migrate data via endpoint

--- app/src/main/java/de/snookersbuddy/snookersbuddyserver/ports/rest/admin/TableController.java
+++ app/src/main/java/de/snookersbuddy/snookersbuddyserver/ports/rest/admin/TableController.java
@@ -5,7 +5,10 @@ import de.snookersbuddy.snookersbuddyserver.application.item.ItemService;
 import de.snookersbuddy.snookersbuddyserver.application.option.OptionService;
 import de.snookersbuddy.snookersbuddyserver.application.variant.VariantService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.flywaydb.core.Flyway;
 
 @RestController
 public class TableController {
@@ -28,5 +31,22 @@ public class TableController {
     @GetMapping("/api/table-data")
     public GetTableDataOutput getOptions() {
         return this.itemService.getTableData();
+    }
+
+    @PostMapping("/api/reset-data")
+    public void resetData() {
+        Flyway flyway =
+            Flyway.configure()
+            // FIXME: Not suited for production
+            .dataSource("jdbc:postgresql://localhost:25432/snookers" , "snookers" , "snookers")
+            // TODO: Should be moved to properties and set to true in production
+            .cleanDisabled(false)
+            .load()
+            ;
+
+        // Start the migration
+        flyway.clean();
+        flyway.migrate();
+
     }
 }
